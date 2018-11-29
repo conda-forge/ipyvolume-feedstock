0eb7977f57b237923e8f7282d15bdb71cc0967dc
Hadrien Mary <hadrien.mary@gmail.com>
2018-11-29 10:38:00 -0500

    Turn echo on during Windows activation to debug more easily in case of errors.

--- recipe/post-link.bat
+++ recipe/post-link.bat
@@ -1,3 +1,4 @@
-@echo off
+@echo on
 
-"%PREFIX%\Scripts\jupyter-nbextension.exe" enable ipyvolume --py --sys-prefix > NUL 2>&1 && if errorlevel 1 exit 1
+"%PREFIX%\Scripts\jupyter-nbextension.exe" enable ipyvolume --py --sys-prefix > "%PREFIX%\.messages.txt" 2>&1
+if errorlevel 1 exit 1
